52f249e619e4b211c6b692af8ec93333ac9e6b0b
Maxim <grubenko.m@ya.ru>
2022-04-06 00:17:12 +0300

    Implemented a doubly linked list. The prefix increment of the iterator does not work correctly

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <list>
+
+#include "MyList.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    MyList<int> myList;
+
+    myList.push_back(7);
+    myList.push_back(5);
+    myList.push_back(10);
+    myList.push_back(-1);
+    myList.push_back(6);
+
+//    auto it = myList.begin();
+//    auto itend = myList.end();
+//    std::cout << "begin: " << it.get_current().data << " end: " << itend.get_current().data << std::endl;
+
+    for (auto i : myList) {
+        std::cout << i.data << std::endl;
+    }
     return 0;
 }
